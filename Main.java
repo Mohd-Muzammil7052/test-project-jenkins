993682b6f0cee5196b86801b0badbbfa14dab3c0
Mohd Muzammil <158077397+Mohd-Muzammil7052@users.noreply.github.com>
2026-04-21 15:08:41 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins...");
+        System.out.println("Hello from Jenkins again...");
+        System.out.println(new Date());
+    }
+}
